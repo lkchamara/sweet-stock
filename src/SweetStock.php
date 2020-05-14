14a985473f7da153142449865c0c6d9bab98db84
chamaramaduranga <lkchamara@gmail.com>
2020-05-14 22:25:52 +0530

    code refactoring

--- src/SweetStock.php
+++ src/SweetStock.php
@@ -23,26 +23,16 @@ class SweetStock
 
     public function tick()
     {
+        $this->quality = (($this->name == 'Aged Brie' or $this->name == 'Backstage passes to a TAFKAL80ETC concert') and $this->quality < 50) ? ++$this->quality : $this->quality;
+        $this->quality = ($this->name == 'Backstage passes to a TAFKAL80ETC concert' and $this->quality < 50) ? (($this->sellIn < 11) ? ($this->sellIn < 6) ? $this->quality+=2 : ++$this->quality : $this->quality) : $this->quality;
 
-        $backstage = 'Backstage passes to a TAFKAL80ETC concert';
-        $normal = 'normal';
-        $agedBrie = 'Aged Brie';
-        $sulfuras = 'Sulfuras, Hand of Ragnaros';
-        $conjured = 'Conjured Mana Cake';
+        $this->sellIn = ($this->name != 'Sulfuras, Hand of Ragnaros') ? --$this->sellIn : $this->sellIn;
 
-        $this->quality = ($this->name == $backstage and $this->quality < 50)?(($this->sellIn < 11)?(($this->sellIn < 6)?$this->quality+=2:++$this->quality):$this->quality):$this->quality;
+        $this->quality = ($this->sellIn < 0 and $this->name == 'Aged Brie' and $this->quality < 50) ? ++$this->quality : $this->quality;
+        $this->quality = ($this->sellIn < 0 and $this->name == 'Backstage passes to a TAFKAL80ETC concert') ? 0 : $this->quality;
 
-        $this->sellIn = ($this->name != $sulfuras) ? --$this->sellIn : $this->sellIn;
+        $this->quality = ($this->name == 'normal' and $this->quality > 0) ? (($this->sellIn < 0) ? $this->quality-=2 : --$this->quality) : $this->quality;
 
-        $this->quality = ($this->name == $normal and $this->quality > 0)?(($this->sellIn < 0)?$this->quality-=2:--$this->quality):$this->quality;
-
-        $this->quality = (($this->name == $agedBrie or $this->name == $backstage) and $this->quality < 50) ? ++$this->quality : $this->quality;
-
-        $this->quality = ($this->name == $agedBrie and $this->quality < 50 and $this->sellIn < 0) ? ++$this->quality : $this->quality;
-
-        $this->quality = ($this->name == $backstage and $this->sellIn < 0) ? $this->quality=0 : $this->quality;
-
-        $this->quality = ($this->name == $conjured and $this->quality > 0)?(($this->sellIn < 0)?$this->quality-=4:$this->quality-=2):$this->quality;
-
+        $this->quality = ($this->name == 'Conjured Mana Cake' and $this->quality > 0)?(($this->sellIn < 0)?$this->quality-=4:$this->quality-=2):$this->quality;
     }
 }
